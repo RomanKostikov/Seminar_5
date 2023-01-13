629c562ec77e32fce8dd5e21b3e576feba25c7a4
Roman <romankos1993@gmail.com>
2023-01-13 21:59:16 +0300

    task 003 completed

---
+++ task003/task003.py
@@ -0,0 +1,18 @@
+# 38. Напишите программу, удаляющую из текста все слова, содержащие "абв".
+"""Doc."""
+
+
+def del_some_words(my_text):
+    my_text = list(filter(lambda x: 'абв' not in x, my_text.split()))
+    return " ".join(my_text)
+
+
+def main():
+    my_text = 'Напишите напиабв програбвмму программу, удаляющую из \
+    этого абв текста все абв вабвс слова, содерабващие содержащие "абв"'
+    my_text = del_some_words(my_text)
+    print(my_text)
+
+
+if __name__ == '__main__':
+    main()
