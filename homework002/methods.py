8763df351b905a95cb69bfba79fe9cb00b910785
Roman <romankos1993@gmail.com>
2023-01-18 22:16:24 +0300

    homework 002 completed

---
+++ homework002/methods.py
@@ -0,0 +1,47 @@
+"""Doc."""
+from random import randint
+
+
+def move_player(name_player, candies, max_move):
+    valid = False
+    while not valid:
+        move = input(f'{name_player}, Ваш ход: ')
+        try:
+            move = int(move)
+            if move > 0 and move <= max_move and move <= candies:
+                print(f'Вы забрали {move} конфет')
+                candies -= move
+                print(f'Осталось {candies} конфет')
+                valid = True
+            else:
+                print('Количество взятых конфет должно быть в '
+                      f'интервале от 1 до {max_move} или не больше '
+                      'оставшегося количества конфет')
+        except ValueError:
+            print('Необходимо ввести целое число.')
+    return candies
+
+
+def move_stupid_bot(candies, max_move):
+    move = randint(1, max_move) if candies >= max_move else randint(1, candies)
+    print(f'Бот забрал {move} конфет')
+    candies -= move
+    print(f'Осталось {candies} конфет')
+    return candies
+
+
+def move_smart_bot(candies, max_move):
+    move = candies % (max_move + 1)
+    if move == 0:
+        move = randint(1, max_move) if candies >= max_move else candies
+    print(f'Бот забрал {move} конфет')
+    candies -= move
+    print(f'Осталось {candies} конфет')
+    return candies
+
+
+def check_win(candies, determing_moves, first_name, second_name):
+    if candies == 0:
+        return first_name if determing_moves % 2 == 0 else second_name
+    else:
+        return False
